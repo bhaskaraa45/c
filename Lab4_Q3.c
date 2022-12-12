e1223c63f7e47f4905ae2816ea89f13c149a9e6a
Bhaskar Mandal <bhaskarmandal369@gmail.com>
2022-12-12 11:39:40 +0530

    Create Lab4_Q3.c

---
+++ Lab4_Q3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main() {
+  int n, reversed ,og;
+    printf("Enter an integer: ");
+    scanf("%d", &n);
+    og = n;
+    reversed=0;
+    int remainder;
+    
+    while (n != 0) {
+        remainder = n % 10;
+        reversed = reversed * 10 +remainder ;
+        n /= 10;
+    }
+
+    
+    if (og == reversed)
+        printf("true");
+    else
+        printf("false");
+
+    return 0;
+}
