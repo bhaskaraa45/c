22d90a05540dbb5b8cb53cd826b79e98bcf42134
Bhaskar Mandal <bhaskarmandal369@gmail.com>
2022-12-31 22:03:30 +0530

    privacy

--- Lab2_Q1.c
+++ Lab2_Q1.c
@@ -1,12 +1,3 @@
-#include <stdio.h>
-
-int main(){
-	int marks , b ,c,d;
-	printf("enter your number : \n");
-	scanf("%d", &marks);
-
-	//method_1
-	
 	
 	 if(marks>95){
 		printf("Your grade is : A+");
@@ -25,22 +16,3 @@ int main(){
         }
 	else if (marks>50 && marks<=60){
                 printf("Your grade is : C");
-        }
-        else if (marks>40 && marks<=50){
-                printf("Your grade is : C-");
-        }       
-	else if (marks>30 && marks<=40){
-                printf("Your grade is : D");
-        }
-	else{
-                printf("Your grade is : F");}
-        
-        
-        
-        //method_2
-        //ternary
-        
-       // marks> 95 ? printf("Your grade is : A+") : marks > 90 ? printf("Your grade is : A") : marks > 80 ? printf("Your grade is : A-") : marks > 70 ? printf("Your grade is : B") : marks > 60 ? printf("Your grade is : B-") : marks > 50 ? printf("Your grade is : C") : marks > 40 ? printf("Your grade is : C-") : marks > 30 ? printf("Your grade is : D") : marks >=0 ? printf("Your grade is : F") : printf("Wrong Input") ;
-               
- return 0;
- }
